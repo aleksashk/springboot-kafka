d6901ad39c1e11080c89859465d1bc170a0ae130
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-11-01 18:13:45 +0300

    create simple POJO to Serialize/Deserialize

---
+++ src/main/java/by/flameksandr/kafka/payload/User.java
@@ -0,0 +1,14 @@
+package by.flameksandr.kafka.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private int id;
+    private String firstName;
+    private String lastName;
+}
